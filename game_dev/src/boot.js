eb72884dca1582fb5308ed070b57a04be304ea90
daniji09 <daniji09@ucm.es>
2025-02-26 18:40:48 +0100

    Very simple and initial version of a progress bar created

--- game_dev/src/boot.js
+++ game_dev/src/boot.js
@@ -37,6 +37,11 @@ export default class Boot extends Phaser.Scene {
   preload() {
     // Con setPath podemos establecer el prefijo que se añadirá a todos los load que aparecen a continuación
     //this.load.setPath('assets/sprites/');
+
+    // shows the progress
+    this.progress_bar();  // HE LITERALMENTE COPIADO LA BARRA DEL EJEMPLO DE ARRIBA, SE PODRÍAN PERSONALIZAR UN POCO (SE PUEDEN HACER COSAS MUY CHULAS)
+
+    // loading each sprite
     this.load.image('loading', loading);
     this.load.image('angel', angel);
     this.load.image('seraph', seraph);
@@ -59,5 +64,83 @@ export default class Boot extends Phaser.Scene {
   create() {
     this.add.image(320, 256, 'loading');
     this.scene.start('d1_1');
+  }
+
+  // creates, manages and shows the initial loading progress bar
+  progress_bar(){
+      // atributes needed
+      var width = this.cameras.main.width;
+      var height = this.cameras.main.height;
+
+      let progressBoxWidth = 420;
+      let progressBoxHeigth = 50;
+      let progressBarHeigth = 30;
+
+      // the bar itself: a box as a container and the bar
+      var progressBar = this.add.graphics();
+      var progressBox = this.add.graphics();
+      progressBox.fillStyle(0x222222, 0.8);
+      progressBox.fillRect((width - progressBoxWidth)/2, (height - progressBoxHeigth)/2, progressBoxWidth, progressBoxHeigth);
+
+      // to add loading text
+      var loadingText = this.make.text({
+          x: width / 2,
+          y: height / 2 - progressBoxHeigth,
+          text: 'Loading...',
+          style: {
+              font: '20px monospace',
+              fill: '#ffffff'
+          }
+      });
+      loadingText.setOrigin(0.5, 0.5);
+
+      // to add the percentage completion
+      var percentText = this.make.text({
+          x: width / 2,
+          y: height / 2,
+          text: '0%',
+          style: {
+              font: '18px monospace',
+              fill: '#ffffff'
+          }
+      });
+      percentText.setOrigin(0.5, 0.5);
+
+      // to add each asset loaded
+      var assetText = this.make.text({
+          x: width / 2,
+          y: height / 2 + progressBoxHeigth,
+          text: '',
+          style: {
+              font: '18px monospace',
+              fill: '#ffffff'
+          }
+      });
+      assetText.setOrigin(0.5, 0.5);
+
+      // callback that updates the percentage of completion
+      this.load.on('progress', function (value) {
+          console.log(value);
+          percentText.setText(parseInt(value * 100) + '%');
+          progressBar.clear();
+          progressBar.fillStyle(0xffffff, 1);
+          progressBar.fillRect((width - progressBoxWidth)/2, (height - progressBarHeigth)/2, progressBoxWidth * value, progressBarHeigth);
+      });
+         
+      // callback that shows the last asset loaded succesfully
+      this.load.on('fileprogress', function (file) {
+          console.log(file.src);
+          assetText.setText('Loading asset: ' + file.key);
+      });
+
+      // callback that completes the operation, destroying all the objects on screen
+      this.load.on('complete', function () {
+          console.log('complete');
+          progressBar.destroy();
+          progressBox.destroy();
+          loadingText.destroy();
+          percentText.destroy();
+          assetText.destroy();
+      })
   }
 }
