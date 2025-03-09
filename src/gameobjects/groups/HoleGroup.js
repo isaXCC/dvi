f91af5d96f8036619491b29209ca7aa5016f9377
hibjan <jhibjan@ucm.es>
2025-03-09 20:01:04 +0100

    fix fall

--- src/gameobjects/groups/HoleGroup.js
+++ src/gameobjects/groups/HoleGroup.js
@@ -14,9 +14,10 @@ export default class HoleGroup extends DefaultGroup {
             this.scene.sound.play('fallingSound', { volume: 2 });
             this.falling = true;
             let prev_x =  player.x, prev_y = player.y;
+            let hole_x = hole.x + 32, hole_y = hole.y + 32;
             let speed = player._speed;
             player._speed = 0;
-            player.setPosition((player.x+hole.x)/2, (player.y+hole.y)/2)
+            player.setPosition((player.x+hole_x)/2, (player.y+hole_y)/2)
             this.scene.tweens.add({
                 targets: player,
                 scaleX: 0, 
@@ -25,8 +26,8 @@ export default class HoleGroup extends DefaultGroup {
                 ease: 'Sine',
                 onComplete: () => {
                     player.setScale(58/38, 58/38);
-                    let {x_norm, y_norm} = getNormDist(hole.x, hole.y, prev_x, prev_y);
-                    player.setPosition((x_norm*2)+prev_x, (y_norm*2)+prev_y);
+                    let {x_norm, y_norm} = getNormDist(hole_x, hole_y, prev_x, prev_y);
+                    player.setPosition((x_norm*12)+prev_x, (y_norm*12)+prev_y);
                     player.setSize(24, 25).setOffset(20, 22.5);
                     this.scene.time.delayedCall(PARAMETERS.HOLE.DELAY, () => {
                         player._speed = speed;
