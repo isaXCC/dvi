617a4acbf67ad9177334fb80c36786bb9226087a
hibjan <jhibjan@ucm.es>
2025-04-09 11:56:00 +0200

    d1_test

--- src/boot.js
+++ src/boot.js
@@ -146,7 +146,7 @@ export default class Boot extends Phaser.Scene {
    */
   create() {
     this.add.image(320, 256, 'loading');
-    this.scene.start('test');
+    this.scene.start('d1_test');
   }
 
   // creates, manages and shows the initial loading progress bar
