c7cc05c3e179852e5069a020f056fda63c460f67
hibjan <jhibjan@ucm.es>
2025-05-07 12:07:05 +0200

    NOT ammoup

--- src/gameobjects/powerups/ammoup.js
+++ src/gameobjects/powerups/ammoup.js
@@ -5,7 +5,6 @@ export default class AmmoUp extends PowerUp {
     constructor(player, scene, x, y){
         super(player, scene, x, y, 'ammoup');
         this._isStored = false;
-        this.setFrame(2);
     }
 
     effect(){
