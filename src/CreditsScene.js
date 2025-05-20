9445afdbdb525d363fb1e8925f645722bb9f6809
daniji09 <daniji09@ucm.es>
2025-05-20 13:18:40 +0200

    Credits Scene finished

--- src/CreditsScene.js
+++ src/CreditsScene.js
@@ -9,14 +9,234 @@ export default class CreditsScene extends Phaser.Scene {
         // credit content
         const creditsText = `
 CREDITS
-————————————
-Programming   ·  Bla
-Art           ·  Bla
-Music         ·  Bla
+————————————————————————————————————
 
-Special Thanks
-————————
+
+GAME DIRECTION
+——————————————————
+
+Game Directors
+—————————
+Isabel Xian Cardoso Cerón
+Juan Andrés Hibjan Cardona
+Daniel Jiménez Caballero
+Leonardo Prado de Souza
+
+Creative Directors
+—————————
+Isabel Xian Cardoso Cerón
+Leonardo Prado de Souza
+
+Technical Directors
+—————————
+Juan Andrés Hibjan Cardona
+Daniel Jiménez Caballero
+Leonardo Prado de Souza
+
+Desing Director
+—————————
+Leonardo Prado de Souza
+
+
+PRODUCTION
+——————————————————
+
+Executive Producers
+—————————
+Isabel Xian Cardoso Cerón
+Juan Andrés Hibjan Cardona
+Daniel Jiménez Caballero
+Leonardo Prado de Souza
+
+Producers
+—————————
+Isabel Xian Cardoso Cerón
+Juan Andrés Hibjan Cardona
+Daniel Jiménez Caballero
+Leonardo Prado de Souza
+
+Project Managers
+—————————
+Isabel Xian Cardoso Cerón
+Juan Andrés Hibjan Cardona
+Daniel Jiménez Caballero
+Leonardo Prado de Souza
+
+Development Coordinators
+—————————
+Isabel Xian Cardoso Cerón
+Juan Andrés Hibjan Cardona
+Daniel Jiménez Caballero
+Leonardo Prado de Souza
+
+
+GAME DESIGN
+——————————————————
+
+Lead Game Designers
+—————————
+Juan Andrés Hibjan Cardona
+Daniel Jiménez Caballero
+Leonardo Prado de Souza
+
+Systems Designers
+—————————
+Juan Andrés Hibjan Cardona
+Daniel Jiménez Caballero
+Leonardo Prado de Souza
+
+Level Designers
+—————————
+Juan Andrés Hibjan Cardona
+Daniel Jiménez Caballero
+Leonardo Prado de Souza
+
+Combat Designers
+—————————
+Juan Andrés Hibjan Cardona
+Leonardo Prado de Souza
+
+UX Designer
+—————————
+Daniel Jiménez Caballero
+
+
+PROGRAMMING
+——————————————————
+
+Lead Programmers
+—————————
+Juan Andrés Hibjan Cardona
+Daniel Jiménez Caballero
+Leonardo Prado de Souza
+
+Gameplay Programmers
+—————————
+Juan Andrés Hibjan Cardona
+Daniel Jiménez Caballero
+Leonardo Prado de Souza
+
+AI Programmers
+—————————
+Juan Andrés Hibjan Cardona
+Daniel Jiménez Caballero
+Leonardo Prado de Souza
+
+Physics Programmers
+—————————
+Juan Andrés Hibjan Cardona
+Daniel Jiménez Caballero
+Leonardo Prado de Souza
+
+UI Programmers
+—————————
+Isabel Xian Cardoso Cerón
+Daniel Jiménez Caballero
+
+Optimization Engineers
+—————————
+Juan Andrés Hibjan Cardona
+Daniel Jiménez Caballero
+Leonardo Prado de Souza
+
+
+ART
+——————————————————
+
+Art Director
+—————————
+Isabel Xian Cardoso Cerón
+
+Lead Artist
+—————————
+Isabel Xian Cardoso Cerón
+
+Concept Artist
+—————————
+Isabel Xian Cardoso Cerón
+
+Character Artist
+—————————
+Isabel Xian Cardoso Cerón
+
+Environment Artist
+—————————
+Isabel Xian Cardoso Cerón
+
+Texture Artist
+—————————
+Isabel Xian Cardoso Cerón
+
+Animator
+—————————
+Isabel Xian Cardoso Cerón
+
+Technical Artist
+—————————
+Isabel Xian Cardoso Cerón
+
+Lighting Artist
+—————————
+Isabel Xian Cardoso Cerón
+
+UI/UX Artist
+—————————
+Isabel Xian Cardoso Cerón
+
+
+AUDIO
+——————————————————
+
+Audio Directors
+—————————
+Juan Andrés Hibjan Cardona
+Leonardo Prado de Souza
+
+Sound Designers
+—————————
+Juan Andrés Hibjan Cardona
+Daniel Jiménez Caballero
+Leonardo Prado de Souza
+
+Dialogue Editor
+—————————
+Juan Andrés Hibjan Cardona
+
+Audio Programmers
+—————————
+Juan Andrés Hibjan Cardona
+Daniel Jiménez Caballero
+Leonardo Prado de Souza
+
+
+NARRATIVE
+——————————————————
+
+Narrative Directors
+—————————
+Isabel Xian Cardoso Cerón
+Juan Andrés Hibjan Cardona
+Daniel Jiménez Caballero
+Leonardo Prado de Souza
+
+Lead Writers
+—————————
+Isabel Xian Cardoso Cerón
+Juan Andrés Hibjan Cardona
+Daniel Jiménez Caballero
+Leonardo Prado de Souza
+
+
+SPECIAL THANKS
+————————————————————————————————————
+Our teachers
 Our beta testers
+Our classmates
+Our families
+That random guy I asked once for directions
+The cafeteria bartender
+World-wide cats
+
 
 © 2025 Phat Boi Games
         `;
@@ -35,10 +255,10 @@ Our beta testers
             targets: textObject,
             y: -textObject.height,
             ease: 'Linear',
-            duration: 20000
+            duration: 60000
         });
 
-        this.time.delayedCall(10000, () => this.endCredits());
+        this.time.delayedCall(62000, () => this.endCredits());
     }
 
     endCredits() {
