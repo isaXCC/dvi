3817e1a347c41d7c46d278c739f39e4e2a715dbf
iXian <147181020+isaXCC@users.noreply.github.com>
2025-06-26 11:54:08 +0200

    start

--- src/StartMenu.js
+++ src/StartMenu.js
@@ -79,7 +79,7 @@ export default class StartMenu extends Phaser.Scene {
             this.fade_to_black();
 
             // and then, changes scene
-            this.time.delayedCall(100, () => {this.scene.start('d2_3');}, [], this);
+            this.time.delayedCall(100, () => {this.scene.start('d1_1');}, [], this);
         });
 
     }
